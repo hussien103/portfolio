3b98851395a76c7e561b3e5fdb3f42ad98be37eb
hussien103 <2000459@eng.asu.edu.eg>
2024-08-05 16:12:18 +0300

    added contact

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub, faTwitter,} from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faPhone, faMapMarker } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-footer',
   standalone: true,
@@ -12,4 +13,7 @@ export class FooterComponent {
   faLinkedin = faLinkedin;
   faGithub = faGithub;
   faTwitter = faTwitter;  
+  faEnvelope = faEnvelope;
+  faPhone = faPhone;
+  faMapMarker = faMapMarker;
 }
